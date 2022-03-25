687075bc5ee183eb9c9ed761558f51538ddd16fc
CenZo128 <vincentguizot128@gmail.com>
2022-03-25 20:13:33 +0700

    update routes

--- server/routes/document.js
+++ server/routes/document.js
@@ -1,5 +1,6 @@
 const documentRoute = require('express').Router()
 const DocumentController = require('../controllers/DocumentController')
+// const { authentication } = require('../middlewares/auth')
 
 documentRoute.get('/', DocumentController.allDocuments)
 documentRoute.get('/info/:id', DocumentController.info)
